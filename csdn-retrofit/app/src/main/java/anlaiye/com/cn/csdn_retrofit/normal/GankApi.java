6d8d8b3c556345fd82c05b7aa6b5b2d3394ffbcc
mcxtzhang <mcxtzhang@163.com>
2017-02-21 23:09:07 +0800

    2-2

--- csdn-retrofit/app/src/main/java/anlaiye/com/cn/csdn_retrofit/normal/GankApi.java
+++ csdn-retrofit/app/src/main/java/anlaiye/com/cn/csdn_retrofit/normal/GankApi.java
@@ -1,8 +1,13 @@
 package anlaiye.com.cn.csdn_retrofit.normal;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Url;
 
 /**
  * 介绍：
@@ -21,4 +26,31 @@ public interface GankApi {
     Call<GetBean> getData(@Path("category") String a,
                           @Path("size") String b,
                           @Path("page") String c);
+
+
+    /**
+     * 数据类型： 福利 | Android | iOS | 休息视频 | 拓展资源 | 前端 | all
+     * 请求个数： 数字，大于0
+     * 第几页：数字，大于0
+     */
+    @GET
+    Call<GetBean> getDataWithUrl(@Url String url);
+
+
+    /**
+     * 提交表单格式如下:
+     * 字段	描述	备注
+     * url	想要提交的网页地址
+     * desc	对干货内容的描述	单独的文字描述
+     * who	提交者 ID	干货提交者的网络 ID
+     * type	干货类型	可选参数: Android | iOS | 休息视频 | 福利 | 拓展资源 | 前端 | 瞎推荐 | App
+     * debug	当前提交为测试数据	如果想要测试数据是否合法, 请设置 debug 为 true! 可选参数: true | false
+     */
+    @FormUrlEncoded
+    @POST("api/add2gank")
+    Call<ResponseBody> postData(@Field("url") String url,
+                                @Field("desc") String desc,
+                                @Field("who") String who,
+                                @Field("type") String type,
+                                @Field("debug") String debug);
 }
--- csdn-retrofit/app/src/main/java/anlaiye/com/cn/csdn_retrofit/normal/NormalActivity.java
+++ csdn-retrofit/app/src/main/java/anlaiye/com/cn/csdn_retrofit/normal/NormalActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import anlaiye.com.cn.csdn_retrofit.R;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -23,18 +24,20 @@ public class NormalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
         mTvResult = (TextView) findViewById(R.id.tvResult);
+        //Step1 拿到Retrofit实例
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://gank.io/")
+                //引入Gson解析库 ，就可以直接以实体的形式拿到返回值
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //Step2 拿到我们的Api 实例
+        final GankApi gankApi = retrofit.create(GankApi.class);
+
         findViewById(R.id.btnGet).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Step1 拿到Retrofit实例
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("http://gank.io/")
-                        //引入Gson解析库 ，就可以直接以实体的形式拿到返回值
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
 
-                //Step2 拿到我们的Api 实例
-                GankApi gankApi = retrofit.create(GankApi.class);
 
                 //Step3 通过Api 拿到 call
                 Call<GetBean> android = gankApi.getData("Android", "10", "1");
@@ -57,6 +60,59 @@ public class NormalActivity extends AppCompatActivity {
                 });
 
 
+            }
+        });
+
+        findViewById(R.id.btnPost).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /**
+                 * 提交表单格式如下:
+                 * 字段	描述	备注
+                 * url	想要提交的网页地址
+                 * desc	对干货内容的描述	单独的文字描述
+                 * who	提交者 ID	干货提交者的网络 ID
+                 * type	干货类型	可选参数: Android | iOS | 休息视频 | 福利 | 拓展资源 | 前端 | 瞎推荐 | App
+                 * debug	当前提交为测试数据	如果想要测试数据是否合法, 请设置 debug 为 true! 可选参数: true | false
+                 */
+                Call<ResponseBody> postData = gankApi.postData("http://square.github.io/retrofit/",
+                        "测试数据",
+                        "未来Android大佬",
+                        "Android",
+                        "true");
+                //Step 4 请求网络
+                postData.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        Toast.makeText(NormalActivity.this, "请求成功", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "onResponse() called with: call = [" + call + "], response = [" + response + "]");
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Toast.makeText(NormalActivity.this, "请求失败:" + t, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        findViewById(R.id.btnUrl).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Step3 通过Api 拿到 call
+                Call<GetBean> android = gankApi.getDataWithUrl("http://gank.io/api/data/Android/10/1");
+
+                android.enqueue(new Callback<GetBean>() {
+                    @Override
+                    public void onResponse(Call<GetBean> call, Response<GetBean> response) {
+                        mTvResult.setText(response.body().getResults().get(0).getDesc());
+                    }
+
+                    @Override
+                    public void onFailure(Call<GetBean> call, Throwable t) {
+
+                    }
+                });
             }
         });
 
